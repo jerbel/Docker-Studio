7720ef6b23269b70e9cdb2b4379f5991dd187812
Christophe Gourdin <christophe.gourdin@inria.fr>
2017-10-11 16:46:31 +0200

    [Feat]Prepare java classes to migrate docker studio v1 on docker studio v2

---
+++ plugins/org.eclipse.cmf.occi.docker.connector/src/org/eclipse/cmf/occi/docker/connector/helpers/DockerContainerHelper.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2016-2017 Inria
+ *  
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * - Christophe Gourdin <christophe.gourdin@inria.fr>
+ *  
+ */
+package org.eclipse.cmf.occi.docker.connector.helpers;
+
+/**
+ * Helpers method to manage containers.
+ * 
+ * @author Christophe Gourdin
+ *
+ */
+public class DockerContainerHelper {
+
+}
---
+++ plugins/org.eclipse.cmf.occi.docker.connector/src/org/eclipse/cmf/occi/docker/connector/synchrone/SynchronizeMachines.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016-2017 Inria
+ *  
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * - Christophe Gourdin <christophe.gourdin@inria.fr>
+ *  
+ */
+package org.eclipse.cmf.occi.docker.connector.synchrone;
+
+/**
+ * Import existing machines and referenced via existing defined accounts models (to design on docker extension, aws accounts, docker cloud accounts, rackspace accounts etc...).
+ * @author Christophe Gourdin
+ *
+ */
+public class SynchronizeMachines {
+
+}
